515a6373c3b4df1d4ae651c8c8589792ef699c54
aigo <dog@gmail.com>
2026-08-07 19:43:42 +0800

    init: springboot3.4.4 docker-agent项目初始化

---
+++ src/main/java/com/aigo/dockeragent/DockerAgentApplication.java
@@ -0,0 +1,13 @@
+package com.aigo.dockeragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aigo/dockeragent/DockerAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aigo.dockeragent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
